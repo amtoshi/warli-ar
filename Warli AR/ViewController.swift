b6fefb5fd7a82faccc96c4c35f5f308ba11ca5a9
Ashutosh Mane <itisashutoshmane@gmail.com>
2022-06-11 06:33:01 +0300

    Initial Commit

---
+++ Warli AR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Warli AR
+//
+//  Created by Ashutosh Mane on 11/06/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
